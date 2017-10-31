43b40d6147a68e5465cc052260fee1a270ec601f
AlekseySkovorodnikov <s.a.m-1992@yandex.ru>
2017-10-31 23:13:41 +0300

    new user

--- users.sql
+++ users.sql
@@ -9,4 +9,5 @@ PRIMARY KEY (`id_user`)
 ) DEFAULT CHARSET=utf8; 
 INSERT INTO `users` (`name`, `age`, `sex`, `city`, `rate`) VALUES 
 ('Aleksey', '1992', 'M', 'Syktyvkar', '1'), 
-('Alexander', '1993', 'M', 'Syktyvkar', '1');
+('Alexander', '1993', 'M', 'Syktyvkar', '1'),
+('Denis','1992', 'M', 'Syktyvkar', '1');
